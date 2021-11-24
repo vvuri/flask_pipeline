8c15508b01a1ee4fff8e15f7e1d5599eeee293f8
Yuri Vinogradov <vvuri1978@gmail.com>
2021-11-24 03:33:42 +0300

    Add FastAPI Swager example

--- app/fapi/main.py
+++ app/fapi/main.py
@@ -1,7 +1,11 @@
+from random import random
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi import HTTPException
 from typing import Optional
+
+from flask import jsonify
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -48,6 +52,92 @@ async def get():
     except IndexError:
         raise HTTPException(404, "Phrase list is empty")
     return phrase
+
+
+@app.get('/random-number', response_class=PlainTextResponse)
+async def random_number():
+    return str(random.randrange(100))
+
+
+@app.get('/alpha')
+async def alpha(text: str):
+    result = {'text': text, 'is_alpha': text.isalpha()}
+    return result
+
+
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    id = request.form.get('id', '0001')
+    name = request.form.get('name', 'Anonymous')
+    # код для аутентификации, валидации, обновления базы данных
+    data = {'id': id, 'name': name}
+    result = {'status_code': '0', 'status_message': 'Success', 'data': data}
+    return jsonify(result)
+
+
+# Обновление языка
+@app.put('/update-language', response_class=PlainTextResponse)
+async def update_language(item: Item):
+    language = item.language
+    return "Successfully updated language to %s" % (language)
+
+
+class Number(BaseModel):
+    value: int
+
+
+class Response(BaseModel):
+    code: int
+    message: str
+    result: bool
+
+
+odd_examples = {
+    "odd": {
+        "summary": "Odd number",
+        "value": {
+            "value": 1111
+        }
+    },
+    "even": {
+        "summary": "Even number",
+        "value": {
+            "value": 322
+        }
+    },
+    "alphabet": {
+        "summary": "Example using alphabet",
+        "description": "Using invalid, non-number input. Will raise `Error: Unprocessable Entity` message.",
+        "value": {
+            "value": "abc"
+        }
+    },
+}
+
+odd_responses = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Odd Number",
+                        "value": {"code": 0, "message": "Success", "result": True}
+                    },
+                    "even": {
+                        "summary": "Even Number",
+                        "value": {"code": 0, "message": "Success", "result": False}
+                    },
+                }
+            }
+        }
+    },
+}
+
+
+@app.post("/odd", response_model=Response, responses=odd_responses)
+def odd(number: Number = Body(..., examples=odd_examples)):
+    return Response(code=200, message="Success", result=number.value % 2)
 
 
 if __name__ == '__main__':
