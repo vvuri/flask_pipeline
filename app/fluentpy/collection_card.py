be356850331adf0c67edd6eacc40b2e88a608d20
Yuri Vinogradov <vvuri1978@gmail.com>
2022-01-08 14:17:46 +0300

    Create fluent python book module

---
+++ app/fluentpy/collection_card.py
@@ -0,0 +1,28 @@
+# Work with collections
+# Run test:
+# $ pytest.\collection_card.py
+
+import collections
+import pytest
+
+Card = collections.namedtuple('Card', {'rank', 'suit'})
+
+
+class FrenchDesk:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades clubs diamonds hearts'.split(' ')
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards(item)
+
+
+def test_one_card():
+    beet_card = Card('7', 'diamonds')
+    print(beet_card)
