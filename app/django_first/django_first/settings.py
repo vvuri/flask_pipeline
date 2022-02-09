541dff6798665fc97321bf96c41f3244ab7b9d6b
Yuri Vinogradov <vvuri1978@gmail.com>
2022-02-09 12:29:24 +0300

    Add bootstrap from static

--- app/django_first/django_first/settings.py
+++ app/django_first/django_first/settings.py
@@ -123,10 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# root ...
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'django_first/static')
         ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
