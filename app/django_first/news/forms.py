eb2c12771f21561b77e47db4d59d2d259d6e4521
Yuri Vinogradov <vvuri1978@gmail.com>
2022-02-17 23:59:58 +0300

    Change form

--- app/django_first/news/forms.py
+++ app/django_first/news/forms.py
@@ -1,29 +1,50 @@
 from django import forms
 
-from .models import Category
+from .models import Category, News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150,
-                            label='Заголовок',
-                            widget=forms.Textarea(attrs={
-                                "class": "form-control",
-                                "style": "height: 60px"
-                            }))
-    content = forms.CharField(label='Текст',
-                              required=False,
-                              widget=forms.Textarea(attrs={
-                                  "class": "form-control",
-                                  "style": "height: 300px"
-                              }))
-    is_publish = forms.BooleanField(label='Опубликовать сразу',
-                                    initial=True,
-                                    widget=forms.CheckboxInput(attrs={
-                                        "class": "form-check-input"
-                                    }))
-    category = forms.ModelChoiceField(queryset=Category.objects.all(),
-                                      label='Категория:',
-                                      empty_label='Категория не выбрана',
-                                      widget=forms.Select(attrs={
-                                          "class": "form-control form-select"
-                                      }))
+# class NewsForm(forms.Form):
+#     title = forms.CharField(max_length=150,
+#                             label='Заголовок',
+#                             widget=forms.Textarea(attrs={
+#                                 "class": "form-control",
+#                                 "style": "height: 60px"
+#                             }))
+#     content = forms.CharField(label='Текст',
+#                               required=False,
+#                               widget=forms.Textarea(attrs={
+#                                   "class": "form-control",
+#                                   "style": "height: 300px"
+#                               }))
+#     is_publish = forms.BooleanField(label='Опубликовать сразу',
+#                                     initial=True,
+#                                     widget=forms.CheckboxInput(attrs={
+#                                         "class": "form-check-input"
+#                                     }))
+#     category = forms.ModelChoiceField(queryset=Category.objects.all(),
+#                                       label='Категория:',
+#                                       empty_label='Категория не выбрана',
+#                                       widget=forms.Select(attrs={
+#                                           "class": "form-control form-select"
+#                                       }))
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_publish', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={"class": "form-control",
+                                            "style": "height: 60px",
+                                            'max_length': 150,
+                                            'label': 'Заголовок'}),
+            'content': forms.Textarea(attrs={"class": "form-control",
+                                             "style": "height: 300px",
+                                             'label': 'Текст',
+                                             'required': False}),
+            'is_publish': forms.CheckboxInput(attrs={"class": "form-check-input"}),
+            'category': forms.Select(attrs={'class': 'form-control'})
+        }
+
+
