1046fd866793acacee9fefcfabb838bd372672c2
Yuri Vinogradov <vvuri1978@gmail.com>
2022-02-18 21:35:41 +0300

    Add ListView for category

--- app/django_first/news/urls.py
+++ app/django_first/news/urls.py
@@ -6,9 +6,13 @@ from news import views
 urlpatterns = [
     # path('', views.index, name='news-list'),
     path('', views.HomeNews.as_view(), name='news-list'),
+
     # path('<int:sign>', views.movie_sign, name='movie-name')
+
+    # path('category/<int:category_id>/', views.get_category, name='news-category'),
+    path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='news-category'),
+
     path('movies', views.movie),
-    path('category/<int:category_id>/', views.get_category, name='news-category'),
     path('<int:news_id>/', views.view_news, name='view_news'),
     path('add-news/', views.add_news, name='add-news')
 ]
--- app/django_first/news/views.py
+++ app/django_first/news/views.py
@@ -22,28 +22,43 @@ class HomeNews(ListView):
         return News.objects.filter(is_publish=True)
 
 
-def index(request):
-    news = News.objects.order_by('-create_at')
-    context = {
-        'news': news,
-        'title': 'Список новостей'
-    }
-    return render(request, template_name='news/index.html', context=context)
+# def index(request):
+#     news = News.objects.order_by('-create_at')
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей'
+#     }
+#     return render(request, template_name='news/index.html', context=context)
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/category.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NewsByCategory, self).get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_publish=True, category_id=self.kwargs['category_id'])
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.filter(pk=category_id)
+#     context = {
+#         'news': news,
+#         'category': category
+#     }
+#     return render(request, template_name='news/category.html', context=context)
 
 
 def movie(request):
     render_page = render_to_string('news/movie_list.html')  # , context=data)
     return HttpResponse(render_page)
-
-
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.filter(pk=category_id)
-    context = {
-        'news': news,
-        'category': category
-    }
-    return render(request, template_name='news/category.html', context=context)
 
 
 def view_news(request, news_id):
